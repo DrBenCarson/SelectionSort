f55951d83aa869c8ef106de319573865a06b034e
DrBenCarson <adrianoz@comcast.net>
2016-12-09 12:11:47 -0700

    Added

---
+++ SelectionSorting/Tester.java
@@ -0,0 +1,26 @@
+
+public class Tester {
+
+	
+		public static void sort(int[]a){
+			int min, minIndex;
+			for (int i=0;i<a.length;++i)
+			{
+				min =a[i];
+				minIndex=i;
+				for (int j=i+1; j<a.length;++j)
+				{
+					if (a[j]<min)
+					{
+						min =a[j];
+						minIndex=j;
+					}
+				}
+				a[minIndex]=a[i];
+				a[i]=min;
+			}
+		}
+
+	
+
+}
